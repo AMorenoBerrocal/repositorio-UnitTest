7fb9b318a837b9f04cd6d5a170d6ba5527f68f73
AVANADE-CORP\a.moreno.berrocal <a.moreno.berrocal@avanade.com>
2024-04-26 08:21:39 +0200

    Comentarios

--- 02/demos/start/EmployeeManagement.Test/EmployeeFactoryTests.cs
+++ 02/demos/start/EmployeeManagement.Test/EmployeeFactoryTests.cs
@@ -13,10 +13,14 @@ namespace EmployeeManagement.Test
         [Fact]
         public void CreateEmployee_ConstructInternalEmployee_SalaryMustBe2500()
         {
+            // Arrange
             var employeeFactory = new EmployeeFactory();
+
+            // Act
             var employee = (InternalEmployee) employeeFactory
                 .CreateEmployee("Ale", "Moreno");
 
+            // Assert
             Assert.Equal(2500, employee.Salary);
 
         }
