1f27e7cbe726866935f59ef9f60b267899760ce7
AVANADE-CORP\a.moreno.berrocal <a.moreno.berrocal@avanade.com>
2024-04-26 08:27:20 +0200

    asserting on booleans

---
+++ 02/demos/start/EmployeeManagement.Test/CourceTests.cs
@@ -0,0 +1,24 @@
+﻿using EmployeeManagement.DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Test
+{
+    internal class CourceTests
+    {
+        public void CourseConstructor_ConstructCourse_IsNewMustBeTrue()
+        {
+            // Arrange
+            // nada
+
+            // Act
+            var course = new Course("Curso de manejo de errores");
+
+            // Assert
+            Assert.True(course.IsNew);
+        }
+    }
+}
