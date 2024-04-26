1cc67ef88a14725c722b80a4de2e0fb66653701b
AVANADE-CORP\a.moreno.berrocal <a.moreno.berrocal@avanade.com>
2024-04-26 08:53:31 +0200

    asserting on numbers

--- 02/demos/start/EmployeeManagement.Test/EmployeeFactoryTests.cs
+++ 02/demos/start/EmployeeManagement.Test/EmployeeFactoryTests.cs
@@ -24,5 +24,31 @@ namespace EmployeeManagement.Test
             Assert.Equal(2500, employee.Salary);
 
         }
+
+        [Fact]
+        public void CreateEmployee_ConstructInternalEmployee_SalaryMsutBeBetween2500And3500()
+        {
+            // Arrange
+            var employeeFactory = new EmployeeFactory();
+
+            // Act
+            var employee = (InternalEmployee)employeeFactory.CreateEmployee("Ale", "Moreno");
+
+            // Assert
+            Assert.True(employee.Salary >= 2500 && employee.Salary <= 3500, "Salario fuera de rango");
+        }
+
+        [Fact]
+        public void CreateEmployee_ConstructInternalEmployee_SalaryMsutBeBetween2500And3500_InRange()
+        {
+            // Arrange
+            var employeeFactory = new EmployeeFactory();
+
+            // Act
+            var employee = (InternalEmployee)employeeFactory.CreateEmployee("Ale", "Moreno");
+
+            // Assert
+            Assert.InRange(employee.Salary, 2500, 3500);
+        }
     }
 }
