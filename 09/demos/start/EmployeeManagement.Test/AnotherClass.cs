ec32b93eafa30cdbcd69f226ec7ca0e0c1e3a3e1
AVANADE-CORP\a.moreno.berrocal <a.moreno.berrocal@avanade.com>
2024-05-02 08:29:34 +0200

    Parallelism in test

---
+++ 09/demos/start/EmployeeManagement.Test/AnotherClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EmployeeManagement.Test
+{
+    [Collection("No parallelism")]
+    public class AnotherClass
+    {
+        [Fact]
+        public void SlowTest2()
+        {
+            Thread.Sleep(5000);
+            Assert.True(true);
+        }
+    }
+}
